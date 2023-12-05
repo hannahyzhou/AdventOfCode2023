49c5c9985b77909e177fc89c797c0b982d5b69a2
hannahyzhou <76719726+hannahyzhou@users.noreply.github.com>
2023-12-05 17:32:44 -0600

    updating 12/3/2023 code

--- 12_3_2023.py
+++ 12_3_2023.py
@@ -1,6 +1,8 @@
 # Advent of Code 2023
 # December 3rd
 
+### PART 1
+
 # convert engine schematic (strings) to a 2d array
 # iterate through each character of each line. If current index is a digit, start recording what number it is until not a digit
 # make sure to stop before out of bounds
@@ -42,8 +44,11 @@ def check_adjacent_squares(l, i):
 schematic_array = []
 part_number_list = []
 
+possible_part_squares = []
+
 LINE_LENGTH = 0 # each row is a line, row length is the num of characters in each line
 NUMBER_LINES = 0 # number of lines there are in the schematic
+
 
 
 with open ('engine_schematic.txt', 'r') as f:
@@ -55,29 +60,60 @@ with open ('engine_schematic.txt', 'r') as f:
             if char is not '\n': row.append(char)
         schematic_array.append(row)
 
+print("LINE LENGTH: ", LINE_LENGTH)
+print("NUMBER LINES: ", NUMBER_LINES)
+
+# creating the possible_part_squares array
+for l in range(NUMBER_LINES):
+    temp_row = []
+    for i in range(LINE_LENGTH):
+        is_possible_part_square = check_adjacent_squares(l, i)
+        if is_possible_part_square:
+            temp_row.append(True)
+        else: temp_row.append(False)
+    possible_part_squares.append(temp_row)
+
+# DEBUGGING: print array of possible parts squares
+for i in range(NUMBER_LINES):
+    for j in range(LINE_LENGTH):
+        if possible_part_squares[i][j]:
+            print("!", end = " ")
+        else: print(".", end = " ")
+    print('\n')
+
+
 i = 0 # current index in the line
 l = 0 # what number line in the file
 
-# traversing left to right, up to down
+# # traversing left to right, up to down
 
-while l < NUMBER_LINES:
-    while i < LINE_LENGTH:
-        if schematic_array[l][i].isnumeric():
-            is_part_number = False
-            curr_number = schematic_array[l][i] # initialize the current number to the first digit seen
-            is_part_number = check_adjacent_squares(l, i)
-            while i < LINE_LENGTH-1 and schematic_array[l][i].isnumeric():
-                if is_part_number is True: # if we know it's a part number
-                    while i < LINE_LENGTH-1 and schematic_array[l][i].isnumeric(): # keep iterating
-                        i+=1
-                        curr_number += schematic_array[l][i]
-                    # after it's found the full number
-                    part_number_list.append(curr_number)
-                    curr_number = 0 # reset current number to 0
-                else: # if not yet discovered
-                    while i < LINE_LENGTH-1 and schematic_array[l][i].isnumeric(): # keep iterating
-                        i+=1
-                        curr_number += schematic_array[l][i]
+while l < NUMBER_LINES: # while indices are in bounds
+    print("l: ", l)
+    while i < NUMBER_LINES:
+        print("i: ", i)
+        is_part_number = False
+        if schematic_array[l][i].isnumeric(): # if found a number?
+            curr_number = ""
+            while i < NUMBER_LINES and schematic_array[l][i].isnumeric(): # continue iterating until non numeric or OOB
+                curr_number += schematic_array[l][i] # add digit to current number
+                if possible_part_squares[l][i]: # if current digit is adjacent to a symbol
+                    is_part_number = True
+                i+=1
+            print(curr_number)
+            if is_part_number: part_number_list.append(curr_number)
+            curr_number = "" # clear the current number
+            i+=1
+        else: i+=1
+    i=0
+    l+=1
+
+sum = 0
+for part_number in part_number_list:
+    sum += int(part_number)
+print(sum)
+        
+
+
 
                 
 
